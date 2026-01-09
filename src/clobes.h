7f1a2d27e658eebe88c2d66f76e2989612d329cf
Mauricio-100 <ceoseshell@gmail.com>
2026-01-09 16:53:16 +0100

    Add server configuration and new features

--- src/clobes.h
+++ src/clobes.h
@@ -15,9 +15,11 @@
 #include <sys/wait.h>
 #include <sys/utsname.h>
 #include <sys/sysinfo.h>
-
-// Third-party libs (only curl for now)
+#include <pthread.h>
 #include <curl/curl.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <openssl/hmac.h>
 
 // Version
 #define CLOBES_VERSION "4.0.0"
@@ -30,11 +32,13 @@
 #define MAX_ARGS 128
 #define MAX_PATH 4096
 #define MAX_URL 2048
-#define MAX_HEADERS 50
+#define MAX_CLIENTS 100
+#define BUFFER_SIZE 8192
+#define DEFAULT_PORT 8080
+#define DEFAULT_SSL_PORT 8443
 
-// Colors - COMPLETE SET
+// Colors
 #define COLOR_RESET     "\033[0m"
-#define COLOR_BLACK     "\033[30m"
 #define COLOR_RED       "\033[31m"
 #define COLOR_GREEN     "\033[32m"
 #define COLOR_YELLOW    "\033[33m"
@@ -42,19 +46,15 @@
 #define COLOR_MAGENTA   "\033[35m"
 #define COLOR_CYAN      "\033[36m"
 #define COLOR_WHITE     "\033[37m"
-#define COLOR_BRIGHT_BLACK   "\033[90m"
 #define COLOR_BRIGHT_RED     "\033[91m"
 #define COLOR_BRIGHT_GREEN   "\033[92m"
 #define COLOR_BRIGHT_YELLOW  "\033[93m"
 #define COLOR_BRIGHT_BLUE    "\033[94m"
-#define COLOR_BRIGHT_MAGENTA "\033[95m"
 #define COLOR_BRIGHT_CYAN    "\033[96m"
 #define COLOR_BRIGHT_WHITE   "\033[97m"
 
 // Styles
 #define STYLE_BOLD      "\033[1m"
-#define STYLE_DIM       "\033[2m"
-#define STYLE_UNDERLINE "\033[4m"
 
 // Log levels
 typedef enum {
@@ -73,6 +73,8 @@ typedef enum {
     CATEGORY_SYSTEM,
     CATEGORY_CRYPTO,
     CATEGORY_DEV,
+    CATEGORY_SERVER,
+    CATEGORY_WEB,
     CATEGORY_UNKNOWN
 } Category;
 
@@ -89,6 +91,25 @@ typedef struct {
     int alias_count;
 } Command;
 
+// Server Configuration
+typedef struct {
+    int port;
+    int ssl_port;
+    char ip_address[64];
+    int max_connections;
+    int timeout;
+    int keep_alive;
+    int worker_threads;
+    char ssl_cert[256];
+    char ssl_key[256];
+    int enable_ssl;
+    int enable_gzip;
+    char web_root[256];
+    int show_access_log;
+    int maintenance_mode;
+    char maintenance_message[1024];
+} ServerConfig;
+
 // Configuration
 typedef struct {
     int timeout;
@@ -98,6 +119,12 @@ typedef struct {
     int colors;
     int progress_bars;
     int verbose;
+    // New features
+    int enable_websocket;
+    int enable_jwt;
+    int enable_cache;
+    int enable_gzip;
+    int enable_proxy;
 } Config;
 
 // Global state
@@ -125,8 +152,14 @@ void print_progress_bar(long current, long total, const char *label);
 
 // HTTP client
 char* http_get_simple(const char *url);
+char* http_get_advanced(const char *url, int show_headers, int follow_redirects, int timeout);
 char* http_post_simple(const char *url, const char *data);
+char* http_post_advanced(const char *url, const char *data, const char *content_type, int show_headers);
 int http_download(const char *url, const char *output, int show_progress);
+
+// Base64 functions
+char* base64_encode(const char *input, size_t length);
+char* base64_decode(const char *input, size_t *output_length);
 
 // Commands
 int cmd_version(int argc, char **argv);
@@ -136,6 +169,19 @@ int cmd_system(int argc, char **argv);
 int cmd_file(int argc, char **argv);
 int cmd_crypto(int argc, char **argv);
 int cmd_dev(int argc, char **argv);
+int cmd_server(int argc, char **argv);
+int cmd_proxy(int argc, char **argv);
+int cmd_jwt(int argc, char **argv);
+int cmd_websocket(int argc, char **argv);
+int cmd_cache(int argc, char **argv);
+
+// Server functions
+int http_server_start(ServerConfig *config);
+void server_stop(int signal);
+void server_handle_client(int client_socket);
+
+// Interactive mode
+int interactive_mode();
 
 // Registry
 void register_commands();
