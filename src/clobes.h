59535841aa598fdd3fa63a5d6bde21bb2a8544f2
Mauricio-100 <ceoseshell@gmail.com>
2026-01-09 15:37:45 +0100

    Update clobes.h

--- src/clobes.h
+++ src/clobes.h
@@ -15,85 +15,36 @@
 #include <sys/wait.h>
 #include <sys/utsname.h>
 #include <sys/sysinfo.h>
-#include <sys/resource.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <pwd.h>
-#include <grp.h>
-#include <locale.h>
-#include <langinfo.h>
-#include <wchar.h>
-#include <wctype.h>
 
-// Third-party libs
+// SEULEMENT curl pour commencer
 #include <curl/curl.h>
-#include <jansson.h>
-#include <openssl/sha.h>
-#include <openssl/md5.h>
-#include <openssl/aes.h>
-#include <openssl/evp.h>
-#include <openssl/rand.h>
-#include <zlib.h>
 
 // Version
 #define CLOBES_VERSION "4.0.0"
 #define CLOBES_CODENAME "Thunderbolt"
 #define CLOBES_BUILD __DATE__ " " __TIME__
 
-// Constants
-#define MAX_CMD_LENGTH 8192
+// Constants - réduisez pour commencer
+#define MAX_CMD_LENGTH 2048
 #define MAX_OUTPUT_SIZE 65536
 #define MAX_ARGS 128
 #define MAX_PATH 4096
-#define MAX_URL 2048
-#define MAX_HEADERS 50
-#define MAX_CONNECTIONS 10
-#define DEFAULT_TIMEOUT 30
-#define CACHE_SIZE 100
-#define VERSION_CHECK_URL "https://api.github.com/repos/gopu-inc/clobes/releases/latest"
 
-// Colors
+// Couleurs simplifiées
 #define COLOR_RESET     "\033[0m"
-#define COLOR_BLACK     "\033[30m"
 #define COLOR_RED       "\033[31m"
 #define COLOR_GREEN     "\033[32m"
 #define COLOR_YELLOW    "\033[33m"
 #define COLOR_BLUE      "\033[34m"
-#define COLOR_MAGENTA   "\033[35m"
 #define COLOR_CYAN      "\033[36m"
 #define COLOR_WHITE     "\033[37m"
-#define COLOR_BRIGHT_BLACK   "\033[90m"
 #define COLOR_BRIGHT_RED     "\033[91m"
 #define COLOR_BRIGHT_GREEN   "\033[92m"
 #define COLOR_BRIGHT_YELLOW  "\033[93m"
-#define COLOR_BRIGHT_BLUE    "\033[94m"
-#define COLOR_BRIGHT_MAGENTA "\033[95m"
 #define COLOR_BRIGHT_CYAN    "\033[96m"
-#define COLOR_BRIGHT_WHITE   "\033[97m"
-
-// Background colors
-#define BG_BLACK   "\033[40m"
-#define BG_RED     "\033[41m"
-#define BG_GREEN   "\033[42m"
-#define BG_YELLOW  "\033[43m"
-#define BG_BLUE    "\033[44m"
-#define BG_MAGENTA "\033[45m"
-#define BG_CYAN    "\033[46m"
-#define BG_WHITE   "\033[47m"
 
 // Styles
 #define STYLE_BOLD      "\033[1m"
-#define STYLE_DIM       "\033[2m"
-#define STYLE_ITALIC    "\033[3m"
-#define STYLE_UNDERLINE "\033[4m"
-#define STYLE_BLINK     "\033[5m"
-#define STYLE_REVERSE   "\033[7m"
-#define STYLE_HIDDEN    "\033[8m"
 
 // Log levels
 typedef enum {
@@ -105,46 +56,13 @@ typedef enum {
     LOG_TRACE
 } LogLevel;
 
-// HTTP methods
-typedef enum {
-    HTTP_GET,
-    HTTP_POST,
-    HTTP_PUT,
-    HTTP_DELETE,
-    HTTP_HEAD,
-    HTTP_OPTIONS,
-    HTTP_PATCH
-} HttpMethod;
-
-// Output formats
-typedef enum {
-    FORMAT_RAW,
-    FORMAT_JSON,
-    FORMAT_XML,
-    FORMAT_YAML,
-    FORMAT_CSV,
-    FORMAT_TABLE,
-    FORMAT_PRETTY,
-    FORMAT_SILENT
-} OutputFormat;
-
-// Command categories
+// Command categories - réduisez
 typedef enum {
     CATEGORY_NETWORK,
     CATEGORY_FILE,
     CATEGORY_SYSTEM,
     CATEGORY_CRYPTO,
     CATEGORY_DEV,
-    CATEGORY_DB,
-    CATEGORY_CLOUD,
-    CATEGORY_DOCKER,
-    CATEGORY_K8S,
-    CATEGORY_MONITOR,
-    CATEGORY_BACKUP,
-    CATEGORY_MEDIA,
-    CATEGORY_TEXT,
-    CATEGORY_MATH,
-    CATEGORY_AI,
     CATEGORY_UNKNOWN
 } Category;
 
@@ -161,82 +79,20 @@ typedef struct {
     int alias_count;
 } Command;
 
-// HTTP request structure
-typedef struct {
-    char url[MAX_URL];
-    HttpMethod method;
-    char *body;
-    size_t body_size;
-    char *headers[MAX_HEADERS];
-    int header_count;
-    int timeout;
-    int follow_redirects;
-    int verify_ssl;
-    char *output_file;
-    int show_progress;
-    OutputFormat format;
-} HttpRequest;
-
-// HTTP response structure
-typedef struct {
-    long status_code;
-    char *body;
-    size_t body_size;
-    double total_time;
-    double connect_time;
-    double start_transfer_time;
-    size_t download_size;
-    char *content_type;
-    char *effective_url;
-} HttpResponse;
-
-// Cache entry
-typedef struct CacheEntry {
-    char key[256];
-    char *value;
-    size_t size;
-    time_t timestamp;
-    time_t expires;
-    struct CacheEntry *next;
-} CacheEntry;
-
 // Configuration
 typedef struct {
-    // Performance
-    int max_connections;
     int timeout;
-    int retry_attempts;
     int cache_enabled;
-    int parallel_downloads;
-    
-    // Network
     char user_agent[128];
-    char default_protocol[16];
-    int dns_cache;
-    int compression;
-    
-    // Security
     int verify_ssl;
-    int max_redirects;
-    int rate_limit;
-    
-    // UI
     int colors;
     int progress_bars;
-    int emoji;
     int verbose;
-    
-    // Features
-    int auto_update;
-    int analytics;
-    int telemetry;
-    int plugins;
 } Config;
 
 // Global state
 typedef struct {
     Config config;
-    CacheEntry *cache;
     int cache_hits;
     int cache_misses;
     long total_requests;
@@ -245,210 +101,39 @@ typedef struct {
     LogLevel log_level;
 } GlobalState;
 
-// Function prototypes
-
-// Logging
+// Function prototypes - SEULEMENT les fonctions que vous utilisez
 void log_message(LogLevel level, const char *format, ...);
-void set_log_level(LogLevel level);
-LogLevel get_log_level_from_string(const char *level_str);
-
-// Output formatting
 void print_success(const char *format, ...);
 void print_error(const char *format, ...);
 void print_warning(const char *format, ...);
 void print_info(const char *format, ...);
 void print_debug(const char *format, ...);
-void print_trace(const char *format, ...);
 void print_banner();
-void print_table_header(const char **headers, int count);
-void print_table_row(const char **cells, int count);
-void print_json(const char *json_str);
-void print_yaml(const char *yaml_str);
-void print_xml(const char *xml_str);
 void print_progress_bar(long current, long total, const char *label);
-void print_spinner(const char *message);
 
-// HTTP client (curl replacement)
-HttpResponse* http_request(HttpRequest *req);
-void http_response_free(HttpResponse *resp);
+// HTTP client
 char* http_get_simple(const char *url);
 char* http_post_simple(const char *url, const char *data);
 int http_download(const char *url, const char *output, int show_progress);
-int http_upload(const char *url, const char *file, const char *field_name);
-char* http_batch(const char **urls, int count, int parallel);
-void http_benchmark(const char *url, int requests, int concurrent);
 
-// Network utilities
-int ping_host(const char *host, int count, int interval);
-int scan_port(const char *host, int port, int timeout);
-int scan_ports(const char *host, int start_port, int end_port, int timeout);
-char* dns_lookup(const char *domain, const char *type);
-char* reverse_dns(const char *ip);
-char* get_public_ip();
-double speed_test(int duration);
-char* whois(const char *domain);
-char* traceroute(const char *host, int max_hops);
+// Commands
+int cmd_version(int argc, char **argv);
+int cmd_help(int argc, char **argv);
+int cmd_network(int argc, char **argv);
+int cmd_system(int argc, char **argv);
+int cmd_file(int argc, char **argv);
+int cmd_crypto(int argc, char **argv);
+int cmd_dev(int argc, char **argv);
 
-// File operations
-long get_file_size(const char *path);
-char* get_file_hash(const char *path, const char *algorithm);
-int compare_files(const char *file1, const char *file2);
-int find_files(const char *directory, const char *pattern, int recursive);
-int backup_file(const char *source, const char *destination);
-int restore_backup(const char *backup, const char *destination);
-char* read_file(const char *path, size_t *size);
-int write_file(const char *path, const char *content, size_t size);
-int append_file(const char *path, const char *content);
-int compress_file(const char *input, const char *output, int level);
-int decompress_file(const char *input, const char *output);
+// Registry
+void register_commands();
+Command* find_command(const char *name);
 
-// System information
-void get_system_info(char *buffer, size_t size);
-void get_cpu_info(char *buffer, size_t size);
-void get_memory_info(char *buffer, size_t size);
-void get_disk_info(char *buffer, size_t size);
-void get_network_info(char *buffer, size_t size);
-void get_process_info(char *buffer, size_t size);
-void get_user_info(char *buffer, size_t size);
-void get_os_info(char *buffer, size_t size);
-void get_kernel_info(char *buffer, size_t size);
-void get_uptime_info(char *buffer, size_t size);
-
-// Crypto utilities
-char* hash_string(const char *input, const char *algorithm);
-char* encrypt_string(const char *input, const char *key, const char *algorithm);
-char* decrypt_string(const char *input, const char *key, const char *algorithm);
-char* generate_password(int length, int symbols, int numbers);
-int validate_password(const char *password);
-char* generate_key(int bits);
-char* sign_data(const char *data, const char *private_key);
-int verify_signature(const char *data, const char *signature, const char *public_key);
-
-// Development tools
-int compile_c(const char *source, const char *output, const char *options);
-int run_tests(const char *test_dir);
-int code_analysis(const char *source);
-int format_code(const char *source, const char *language);
-int generate_docs(const char *source, const char *output_dir);
-int debug_program(const char *program, const char *args);
-int profile_program(const char *program, const char *args);
-
-// Database operations
-int db_query(const char *query, const char *database, OutputFormat format);
-int db_backup(const char *database, const char *output);
-int db_restore(const char *backup, const char *database);
-int db_optimize(const char *database);
-int db_info(const char *database);
-
-// Cloud operations
-int cloud_upload(const char *file, const char *provider, const char *bucket);
-int cloud_download(const char *file, const char *provider, const char *bucket);
-int cloud_list(const char *provider, const char *bucket);
-int cloud_delete(const char *file, const char *provider, const char *bucket);
-
-// Docker operations
-int docker_build(const char *dockerfile, const char *tag);
-int docker_run(const char *image, const char *args);
-int docker_stop(const char *container);
-int docker_ps(int all);
-int docker_logs(const char *container, int follow);
-int docker_exec(const char *container, const char *command);
-
-// Kubernetes operations
-int k8s_apply(const char *manifest);
-int k8s_delete(const char *resource);
-int k8s_get(const char *resource, const char *namespace);
-int k8s_describe(const char *resource, const char *namespace);
-int k8s_logs(const char *pod, const char *container, int follow);
-
-// Monitoring
-int monitor_cpu(int interval, int duration);
-int monitor_memory(int interval, int duration);
-int monitor_disk(int interval, int duration);
-int monitor_network(int interval, int duration);
-int monitor_process(const char *process, int interval, int duration);
-int set_alert(const char *metric, double threshold, const char *action);
-
-// Backup system
-int backup_create(const char *source, const char *destination, const char *type);
-int backup_list(const char *backup_dir);
-int backup_restore(const char *backup, const char *destination);
-int backup_verify(const char *backup);
-int backup_cleanup(const char *backup_dir, int days);
-
-// Media operations
-int convert_image(const char *input, const char *output, const char *format);
-int convert_video(const char *input, const char *output, const char *format);
-int extract_audio(const char *video, const char *output);
-int resize_image(const char *input, const char *output, int width, int height);
-int compress_media(const char *input, const char *output, int quality);
-
-// Text processing
-int search_text(const char *text, const char *pattern, int regex);
-int replace_text(const char *input, const char *output, const char *pattern, const char *replacement);
-int count_words(const char *text);
-int count_lines(const char *text);
-int sort_lines(const char *input, const char *output, int reverse);
-int unique_lines(const char *input, const char *output);
-
-// Math operations
-double calculate_expression(const char *expression);
-int solve_equation(const char *equation, double *roots, int max_roots);
-int generate_statistics(const double *data, int count, double *stats);
-int plot_function(const char *function, const char *output, double xmin, double xmax);
-
-// AI/ML operations
-int train_model(const char *data, const char *model, const char *algorithm);
-int predict(const char *model, const char *input, char *output, size_t size);
-int evaluate_model(const char *model, const char *test_data, double *metrics);
-int generate_text(const char *prompt, char *output, size_t size);
-
-// Utility functions
-char* url_encode(const char *str);
-char* url_decode(const char *str);
-char* base64_encode(const char *input, size_t length);
-char* base64_decode(const char *input, size_t *output_length);
-char* json_pretty(const char *json);
-char* xml_pretty(const char *xml);
-char* csv_to_json(const char *csv);
-char* json_to_csv(const char *json);
-char* yaml_to_json(const char *yaml);
-char* json_to_yaml(const char *json);
-int validate_json(const char *json);
-int validate_xml(const char *xml);
-int validate_yaml(const char *yaml);
-int validate_csv(const char *csv);
-
-// Cache functions
-void cache_init();
-void cache_cleanup();
-void cache_put(const char *key, const char *value, size_t size, time_t ttl);
-char* cache_get(const char *key, size_t *size);
-void cache_remove(const char *key);
-void cache_clear();
-void cache_stats(int *hits, int *misses, int *size);
-
-// Configuration
-int config_load(const char *path, Config *config);
-int config_save(const char *path, const Config *config);
-int config_get_int(const char *key, int *value);
-int config_set_int(const char *key, int value);
-int config_get_string(const char *key, char *value, size_t size);
-int config_set_string(const char *key, const char *value);
-
-// Command registry
-void command_register(Command *cmd);
-Command* command_find(const char *name);
-void command_list_all();
-void command_list_category(Category category);
-int command_execute(const char *name, int argc, char **argv);
-
-// Initialization and cleanup
+// Initialization
 int clobes_init(int argc, char **argv);
 void clobes_cleanup();
-int clobes_run(int argc, char **argv);
 
-// Main entry point
+// Main
 int main(int argc, char **argv);
 
 #endif // CLOBES_PRO_H
