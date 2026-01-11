7937a7036627a54ae0fa292d14a02ba7a393e2af
Mauricio-100 <ceoseshell@gmail>
2026-01-11 14:43:27 +0000

    add: new functionnality

--- src/clobes.h
+++ src/clobes.h
@@ -15,6 +15,13 @@
 #include <sys/wait.h>
 #include <sys/utsname.h>
 #include <sys/sysinfo.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <ifaddrs.h>
 #include <pthread.h>
 #include <curl/curl.h>
 #include <openssl/evp.h>
@@ -22,8 +29,8 @@
 #include <openssl/hmac.h>
 
 // Version
-#define CLOBES_VERSION "4.0.0"
-#define CLOBES_CODENAME "Thunderbolt"
+#define CLOBES_VERSION "4.1.0"
+#define CLOBES_CODENAME "Thunderbolt Plus"
 #define CLOBES_BUILD __DATE__ " " __TIME__
 
 // Constants
@@ -36,6 +43,7 @@
 #define BUFFER_SIZE 8192
 #define DEFAULT_PORT 8080
 #define DEFAULT_SSL_PORT 8443
+#define MAX_MIME_TYPES 50
 
 // Colors
 #define COLOR_RESET     "\033[0m"
@@ -55,6 +63,7 @@
 
 // Styles
 #define STYLE_BOLD      "\033[1m"
+#define STYLE_UNDERLINE "\033[4m"
 
 // Log levels
 typedef enum {
@@ -78,18 +87,30 @@ typedef enum {
     CATEGORY_UNKNOWN
 } Category;
 
-// Command structure
+// MIME type structure
 typedef struct {
-    char name[32];
-    char description[256];
-    char usage[512];
-    Category category;
-    int min_args;
-    int max_args;
-    int (*handler)(int, char**);
-    char aliases[5][16];
-    int alias_count;
-} Command;
+    char extension[16];
+    char mime_type[64];
+} MimeType;
+
+// Client connection structure
+typedef struct {
+    int socket;
+    struct sockaddr_in address;
+    time_t connect_time;
+    pthread_t thread;
+    char ip[INET_ADDRSTRLEN];
+} ClientInfo;
+
+// Server session structure
+typedef struct {
+    int server_socket;
+    int running;
+    int client_count;
+    ClientInfo clients[MAX_CLIENTS];
+    pthread_t accept_thread;
+    ServerConfig *config;
+} ServerSession;
 
 // Server Configuration
 typedef struct {
@@ -108,7 +129,30 @@ typedef struct {
     int show_access_log;
     int maintenance_mode;
     char maintenance_message[1024];
+    // New fields
+    int enable_php;
+    char index_files[5][32];
+    char php_cgi_path[256];
+    int enable_directory_listing;
+    char custom_domain[256];
+    int public_url_enabled;
+    int qr_code_enabled;
+    int auto_open_browser;
+    char default_page[256];
 } ServerConfig;
+
+// Command structure
+typedef struct {
+    char name[32];
+    char description[256];
+    char usage[512];
+    Category category;
+    int min_args;
+    int max_args;
+    int (*handler)(int, char**);
+    char aliases[5][16];
+    int alias_count;
+} Command;
 
 // Configuration
 typedef struct {
@@ -136,7 +180,19 @@ typedef struct {
     double total_request_time;
     int debug_mode;
     LogLevel log_level;
+    ServerSession *server_session;
 } GlobalState;
+
+// HTTP Request structure
+typedef struct {
+    char method[16];
+    char path[1024];
+    char version[16];
+    char query_string[1024];
+    char headers[10][256];
+    int header_count;
+    char body[8192];
+} HttpRequest;
 
 // Function prototypes
 
@@ -161,6 +217,22 @@ int http_download(const char *url, const char *output, int show_progress);
 char* base64_encode(const char *input, size_t length);
 char* base64_decode(const char *input, size_t *output_length);
 
+// Server functions
+int cmd_server(int argc, char **argv);
+int http_server_start(ServerConfig *config);
+void http_server_stop(ServerSession *session);
+void server_handle_request(int client_socket, ServerConfig *config);
+int parse_http_request(const char *request, HttpRequest *req);
+void serve_static_file(int client_socket, const char *file_path, ServerConfig *config);
+void execute_php(int client_socket, const char *php_file, const char *query_string, ServerConfig *config);
+void generate_directory_listing(int client_socket, const char *dir_path, const char *request_path, ServerConfig *config);
+const char* get_mime_type(const char *filename);
+void url_decode(char *dst, const char *src);
+char* get_local_ip();
+void generate_qr_code(const char *url);
+void open_browser(const char *url);
+char* find_index_file(const char *dir_path, ServerConfig *config);
+
 // Commands
 int cmd_version(int argc, char **argv);
 int cmd_help(int argc, char **argv);
@@ -169,16 +241,10 @@ int cmd_system(int argc, char **argv);
 int cmd_file(int argc, char **argv);
 int cmd_crypto(int argc, char **argv);
 int cmd_dev(int argc, char **argv);
-int cmd_server(int argc, char **argv);
 int cmd_proxy(int argc, char **argv);
 int cmd_jwt(int argc, char **argv);
 int cmd_websocket(int argc, char **argv);
 int cmd_cache(int argc, char **argv);
-
-// Server functions
-int http_server_start(ServerConfig *config);
-void server_stop(int signal);
-void server_handle_client(int client_socket);
 
 // Interactive mode
 int interactive_mode();
@@ -191,7 +257,15 @@ Command* find_command(const char *name);
 int clobes_init(int argc, char **argv);
 void clobes_cleanup();
 
+// Utility functions
+int file_exists(const char *path);
+int dir_exists(const char *path);
+long get_file_size(const char *path);
+
 // Main
 int main(int argc, char **argv);
+
+// Global state
+extern GlobalState g_state;
 
 #endif // CLOBES_PRO_H
